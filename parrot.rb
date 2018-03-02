f7bc511563317819ecc2e32c5734380fde6b8f05
Obed Lorisson <obed.lorisson@gmail.com>
2018-03-02 01:10:36 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,11 +1,12 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(phrase = nil)
+def parrot(arg = nil)
     phrase ="Squawk!"
     puts phrase 
-    return phrase if arg
+    return phrase if arg != nil
 end
+
 
 
 
